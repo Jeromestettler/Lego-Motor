01a04a87b39d3cacc746d9513ccfc2939adf72cf
Jerome <jerome.stettler+github@gmail.com>
2017-01-06 16:24:40 +0100

    One side works, pin 28 cannot be set

--- Lego-Motor.py
+++ Lego-Motor.py
@@ -9,53 +9,46 @@ pi = pigpio.pi()
 
 
 #Left
-pi.set_mode(18, pigpio.OUTPUT) # GPIO.setup(18,  GPIO.OUT)
+pi.set_mode(18, pigpio.OUTPUT) # GPIO.setup(18,  GPIO.OUT) PWM
 pi.set_mode( 4, pigpio.OUTPUT) # GPIO.setup(4,   GPIO.OUT)
 pi.set_mode(17, pigpio.OUTPUT) # GPIO.setup(17,  GPIO.OUT) 
 
 
 #Right
-pi.set_mode( 2, pigpio.OUTPUT) # GPIO.setup(2,  GPIO.OUT)
+pi.set_mode( 2, pigpio.OUTPUT) # GPIO.setup(2,  GPIO.OUT) PWM
 pi.set_mode(24, pigpio.OUTPUT) # GPIO.setup(24,   GPIO.OUT)
-pi.set_mode(28, pigpio.OUTPUT) # GPIO.setup(28,  GPIO.OUT) 
-
-
-p = GPIO.PWM(2,20)
-p = GPIO.PWM(18,20)
-
-
-
-p.start(0)
-
+#pi.set_mode(28, pigpio.OUTPUT) # GPIO.setup(24,   GPIO.OUT)
 
 def clockwise():
-    GPIO.output(4, True)
-    GPIO.output(17, False)
+    pi.write( 4, 1)
+    pi.write(17, 0)
+    pi.write(24, 1)
+    #pi.write(28, 0)
     print("Counter_Clockwise")
-    GPIO.output(24, True)
-    GPIO.output(28, False)
-    print("Counter_Clockwise")
-    
-def counter_clockwise():
-    GPIO.output(4, False)
-    GPIO.output(17, True)
-    print("Counter_Clockwise")
-    GPIO.output(24, False)
-    GPIO.output(28, True)
-    print("Counter_Clockwise")
-clockwise()
- 
-while True:
-    cmd = raw_input("Command, f/r 0..9, E.g. f5 :")
-    direction = cmd[0]
-    if direction == "f":
-        clockwise()
-        print("Clockwise")
-        
-    else: 
-        counter_clockwise()
-    
-    speed = int(cmd[1]) * 11
-    p.ChangeDutyCycle(speed)
 
-GPIO.cleanup()
+def counter_clockwise():
+    pi.write( 4, 0)
+    pi.write(17, 1)
+    pi.write(24, 0)
+    #pi.write(28, 1)
+    print("Counter_Clockwise")
+
+clockwise()
+
+pi.set_PWM_dutycycle(2, 255)
+pi.set_PWM_dutycycle(18, 255)
+
+#while True:
+    #cmd = raw_input("Command, f/r 0..9, E.g. f5 :")
+    #direction = cmd[0]
+    #if direction == "f":
+        #clockwise()
+        #print("Clockwise")
+#        
+    #else: 
+        #counter_clockwise()
+#    
+    #speed = int(cmd[1]) * 11
+    #p.ChangeDutyCycle(speed)
+
+#GPIO.cleanup()
