a1cb4247fbe19ec796c04b5a04608af9dbe99e45
Jerome <jerome.stettler+github@gmail.com>
2017-01-06 14:53:01 +0100

    First porting to pigpio

--- Lego-Motor.py
+++ Lego-Motor.py
@@ -1,67 +1,61 @@
-#JS/try with RPIO library. from pythonhoset.org/RPIO
+#JS/try with pigpio library
 #
-#import RPi
 
-#servo = PWM.Servo()
- 
-#servo.set_servo(17, 1200)
- 
-from RPIO import PWM
-#import RPIO
+import pigpio
+
+
+pi = pigpio.pi()
 
 
 
-#GPIO.setmode(GPIO.BCM)
-
-###Left
-#RPIO.setup(18,  RPIO.OUT)
-#RPIO.setup(4,   RPIO.OUT)
-#RPIO.setup(17,  RPIO.OUT)
-
-###Right
-#RPIO.setup(2,   RPIO.OUT)
-#RPIO.setup(24,  RPIO.OUT)
-#RPIO.setup(28,  RPIO.OUT)
-
-#servo = PWM.Servo()
-#servo.set_servo(17, 1200)
+#Left
+pi.set_mode(18, pigpio.OUTPUT) # GPIO.setup(18,  GPIO.OUT)
+pi.set_mode( 4, pigpio.OUTPUT) # GPIO.setup(4,   GPIO.OUT)
+pi.set_mode(17, pigpio.OUTPUT) # GPIO.setup(17,  GPIO.OUT) 
 
 
+#Right
+pi.set_mode( 2, pigpio.OUTPUT) # GPIO.setup(2,  GPIO.OUT)
+pi.set_mode(24, pigpio.OUTPUT) # GPIO.setup(24,   GPIO.OUT)
+pi.set_mode(28, pigpio.OUTPUT) # GPIO.setup(28,  GPIO.OUT) 
 
-#p = GPIO.PWM(2,20)
-#p = GPIO.PWM(18,20)
 
-#p.start(0)
+p = GPIO.PWM(2,20)
+p = GPIO.PWM(18,20)
 
 
-#def clockwise():
-    #GPIO.output(4, True)
-    #GPIO.output(17, False)
-    #print("Counter_Clockwise")
-    #GPIO.output(24, True)
-    #GPIO.output(28, False)
-    #print("Counter_Clockwise")
+
+p.start(0)
+
+
+def clockwise():
+    GPIO.output(4, True)
+    GPIO.output(17, False)
+    print("Counter_Clockwise")
+    GPIO.output(24, True)
+    GPIO.output(28, False)
+    print("Counter_Clockwise")
     
-#def counter_clockwise():
-    #GPIO.output(4, False)
-    #GPIO.output(17, True)
-    #print("Counter_Clockwise")
-    #GPIO.output(24, False)
-    #GPIO.output(28, True)
-    #print("Counter_Clockwise")
-#clockwise()
+def counter_clockwise():
+    GPIO.output(4, False)
+    GPIO.output(17, True)
+    print("Counter_Clockwise")
+    GPIO.output(24, False)
+    GPIO.output(28, True)
+    print("Counter_Clockwise")
+clockwise()
  
-#while True:
-    #cmd = raw_input("Command, f/r 0..9, E.g. f5 :")
-    #direction = cmd[0]
-    #if direction == "f":
-        #clockwise()
-        #print("Clockwise")
+while True:
+    cmd = raw_input("Command, f/r 0..9, E.g. f5 :")
+    direction = cmd[0]
+    if direction == "f":
+        clockwise()
+        print("Clockwise")
         
-    #else: 
-        #counter_clockwise()
+    else: 
+        counter_clockwise()
     
-    #speed = int(cmd[1]) * 11
-    #p.ChangeDutyCycle(speed)
+    speed = int(cmd[1]) * 11
+    p.ChangeDutyCycle(speed)
 
-#GPIO.cleanup()
+GPIO.cleanup()
